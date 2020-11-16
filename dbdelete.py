9fb7200e8f4dd8289803a9ef70acd40c9d6ca866
prishagoel <72732566+prishagoel@users.noreply.github.com>
2020-11-16 10:59:40 +0530

    Delete dbdelete.py

--- dbdelete.py
+++
@@ -1,8 +0,0 @@
-import mysql.connector
-from dbinfo import *
-
-f = mysql.connector.connect(host = "localhost", user = db_username, passwd = db_password)
-mycur = f.cursor()
-mycur.execute("drop database one_stop_healthcare;")
-
-print("The database has been deleted sucessfully! Thank you for your valuable time!")
