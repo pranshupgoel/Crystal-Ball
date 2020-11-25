f670d772957741a760e10646720bf465231ae7f1
prishagoel <72732566+prishagoel@users.noreply.github.com>
2020-11-25 11:11:55 +0530

    Update dbtest.py

--- dbtest.py
+++ dbtest.py
@@ -1,6 +1,10 @@
 from dbinfo import *
 import mysql.connector
 
-f = mysql.connector.connect(host = "localhost", user = db_username, passwd = db_password)
+try:
+    f = mysql.connector.connect(host = "localhost", user = db_username, passwd = db_password)
+except:
+    print("Your database username or password is wrong. Please go to dbinfo and change it to the correct username and password.")
+else:
+    print("The username and password is correct and now you can move forward! :)")
 
-print("The username and password is correct and now you can move forward! :)")
